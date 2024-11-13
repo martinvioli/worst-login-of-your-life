241b15a9c4163ba742dae7e708a53183665e3efc
sbonvehi97 <sbonvehi@gen-it.com.ar>
2024-11-13 13:23:15 -0300

    FEAT[LOGIN] | Change textarea styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,23 +13,55 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "10px",
+      }}
+    >
       <textarea
-        placeholder="Username"
-        rows={100}
+        placeholder={"Username".split("").join("\n")}
+        rows={username.length || "Username".length}
         cols={1}
-        style={ {color: "black"} }
+        style={{
+          color: "black",
+          resize: "none",
+        }}
         value={username.split("").join("\n")}
-        onChange={(e) => setUsername(e.target.value.replace(/(\r\n|\n|\r)/gm, ""))}
+        onChange={(e) =>
+          setUsername(e.target.value.replace(/(\r\n|\n|\r)/gm, ""))
+        }
       />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+      <textarea
+        placeholder={"Password".split("").join("\n")}
+        rows={password.length || "Password".length}
+        cols={1}
+        style={{
+          color: "black",
+          resize: "none"
+        }}
+        value={password.split("").join("\n")}
+        onChange={(e) =>
+          setPassword(e.target.value.replace(/(\r\n|\n|\r)/gm, ""))
+        }
       />
-      <button onClick={handleLogin}>Login</button>
+      <button
+        onClick={handleLogin}
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+          backgroundColor: "red",
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 };
