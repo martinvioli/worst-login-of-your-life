ba01db995b27a06eb3fc010a200b1f27301fd8dd
Martin Violi <mvioli@gen-it.com.ar>
2024-11-28 10:22:57 -0300

    Finished babys

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,9 +7,92 @@ const Login = () => {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorCount, setErrorCount] = useState(0);
 
   const handleLogin = () => {
-    router.push("/");
+    if (!username || !password) {
+      alert("✅ los campos son obligaortorios!");
+      resetFields();
+      return;
+    }
+    if (username.includes("@")) {
+      alert("✅ no es nesesario agregar el @ al email");
+      resetFields();
+      return;
+    }
+    if (password.length < 12) {
+      alert("✅la contrasenia debe tener al menos 12 caracteres");
+      resetFields();
+      return;
+    }
+
+    const uppercaseCount = password
+      .split("")
+      .filter(
+        (char) => char === char.toUpperCase() && /[A-Z]/.test(char)
+      ).length;
+    if (uppercaseCount < 3) {
+      alert(
+        "✅ la contrasenia debe incluir masomenos 3 mayusculas (Ejemplo de algunas letras mayusculas: A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)"
+      );
+      resetFields();
+      return;
+    }
+
+    const numberCount = password
+      .split("")
+      .filter((char) => /[0-9]/.test(char)).length;
+
+    if (numberCount < 4) {
+      alert("✅ deberia haber al menos 4 numeros. Ejemplo: 1, 3, 4, 5, 8, 9");
+      resetFields();
+      return;
+    }
+
+    if (
+      password.includes("2") ||
+      password.includes("6") ||
+      password.includes("7") ||
+      password.includes("0")
+    ) {
+      alert(
+        "✅ ni el 2, ni el 6, ni el 7, ni el 0 estaban en el ejemplo. para que los agregaste?"
+      );
+      resetFields();
+      return;
+    }
+
+    const specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    const specialCharCount = password
+      .split("")
+      .filter((char) => specialChars.includes(char)).length;
+    if (specialCharCount < 1) {
+      alert(
+        "✅ la pw (password, contraseña en ingles) debe incluir al menos un caracter especial (!, @, #, etc.)."
+      );
+      resetFields();
+      return;
+    }
+    if (errorCount === 0) {
+      setErrorCount((prevCount) => prevCount + 1);
+      alert(
+        "✅ por desgracia ocurrió un error, por favor proba nuevamente de nuevo"
+      );
+      resetFields();
+      return;
+    } else {
+      alert("❌ te logueaste!. Clickea en ACEPTAR para ir al HOME.");
+      setTimeout(() => {
+        window.location.replace(
+          "https://www.youtube.com/watch?v=x1N8VI4kQao&ab_channel=CesarDanielMoro"
+        );
+      }, 500);
+    }
+  };
+
+  const resetFields = () => {
+    setUsername("");
+    setPassword("");
   };
 
   return (
@@ -23,8 +106,8 @@ const Login = () => {
       }}
     >
       <textarea
-        placeholder={"Username".split("").join("\n")}
-        rows={username.length || "Username".length}
+        placeholder={"Email".split("").join("\n")}
+        rows={username.length || "Email".length}
         cols={1}
         style={{
           color: "black",
@@ -41,7 +124,7 @@ const Login = () => {
         cols={1}
         style={{
           color: "black",
-          resize: "none"
+          resize: "none",
         }}
         value={password.split("").join("\n")}
         onChange={(e) =>
